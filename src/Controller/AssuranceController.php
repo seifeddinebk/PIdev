b8062ffed19ad7cb9e41e00a088df8ee45fe149c
neji123 <mohamedneji.benzarti@esprit.tn>
2023-02-17 11:44:23 +0000

    gestion assurance CRUD ADMIN + controle de saisie

---
+++ src/Controller/AssuranceController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Assurance;
+use App\Form\AssuranceType;
+use App\Repository\AssuranceRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/assurance')]
+class AssuranceController extends AbstractController
+{
+    #[Route('/', name: 'app_assurance_index', methods: ['GET'])]
+    public function index(AssuranceRepository $assuranceRepository): Response
+    {
+        return $this->render('assurance/index.html.twig', [
+            'assurances' => $assuranceRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_assurance_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, AssuranceRepository $assuranceRepository): Response
+    {
+        $assurance = new Assurance();
+        $form = $this->createForm(AssuranceType::class, $assurance);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $assuranceRepository->save($assurance, true);
+
+            return $this->redirectToRoute('app_assurance_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('assurance/new.html.twig', [
+            'assurance' => $assurance,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_assurance_show', methods: ['GET'])]
+    public function show(Assurance $assurance): Response
+    {
+        return $this->render('assurance/show.html.twig', [
+            'assurance' => $assurance,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_assurance_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Assurance $assurance, AssuranceRepository $assuranceRepository): Response
+    {
+        $form = $this->createForm(AssuranceType::class, $assurance);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $assuranceRepository->save($assurance, true);
+
+            return $this->redirectToRoute('app_assurance_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('assurance/edit.html.twig', [
+            'assurance' => $assurance,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_assurance_delete', methods: ['POST'])]
+    public function delete(Request $request, Assurance $assurance, AssuranceRepository $assuranceRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$assurance->getId(), $request->request->get('_token'))) {
+            $assuranceRepository->remove($assurance, true);
+        }
+
+        return $this->redirectToRoute('app_assurance_index', [], Response::HTTP_SEE_OTHER);
+    }
+
+   /* #[Route('/afficher', name: 'app_assurance_afficher')]
+    public function afficher(AssuranceRepository $assuranceRepository): Response
+    {
+        return $this->render('assurance/afficher.html.twig', [
+            'assurances' => $assuranceRepository->findAll(),
+        ]);
+    }*/
+}
---
+++ src/Form/AssuranceType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Assurance;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AssuranceType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('region')
+            ->add('telephone')
+            ->add('mail')
+            ->add('adresse')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Assurance::class,
+        ]);
+    }
+}
